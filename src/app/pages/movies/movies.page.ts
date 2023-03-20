347bca29fe42119521d54b3d5b727abd2f47a6ba
diegogglez <diegomiller7@gmail.com>
2023-03-20 18:06:46 +0100

    feat: movie and movie-details pages added

---
+++ src/app/pages/movies/movies.page.ts
@@ -0,0 +1,19 @@
+import { MovieService } from './../../services/movie.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.page.html',
+  styleUrls: ['./movies.page.scss'],
+})
+export class MoviesPage implements OnInit {
+
+  constructor(private movieService: MovieService) { }
+
+  ngOnInit() {
+    this.movieService.getTopRatedMovies().subscribe(res => {
+      console.log(res);
+    })
+  }
+
+}
